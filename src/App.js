ca86191a54a8f4d967fd7e3e66a0d0d18d120db7
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-07-21 19:06:28 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey =
   "cd7e48a92ba80d8b2750f209accad5ce2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
           console.log(articles);
+          setNewsArticles(articles);
         }
       },
     });
   }, []);
 
-  return <div className="App"> My Ai News Application</div>;
+  return (
+    <div className="App">
+      <h1>My Ai News Application</h1>
+      <NewsCards articles={newsArticles} />
+    </div>
+  );
 };
 
 export default App;
