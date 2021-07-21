7a0efb240e8061a910f1ea75f0268663328f7c6b
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-07-21 17:30:56 +0530

    console log latest news headlines

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "cd7e48a92ba80d8b2750f209accad5ce2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
+        }
+      },
+    });
+  }, []);
+
   return <div className="App"> My Ai News Application</div>;
 };
 
