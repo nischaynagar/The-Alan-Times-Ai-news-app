27b2a4c4491761a367936eb7feb9819f7a362e76
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-07-21 15:15:42 +0530

    working on app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <div className="App"> My Ai News Application</div>;
+};
 
 export default App;
